be543ea23b9019810ebac16099198050c1af3e70
Kevin <kevinroosey@gmail.com>
2026-04-28 16:28:10 -0500

    x

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -4,7 +4,6 @@ import { RequireAuth } from "./auth";
 import { AppLayout } from "./routes/app-layout";
 import { LoginRoute } from "./routes/auth/login";
 import { DashboardRoute } from "./routes/dashboard";
-import { HomeRoute } from "./routes/home";
 import { NotFoundRoute } from "./routes/not-found";
 
 export const router = createBrowserRouter([
@@ -14,10 +13,7 @@ export const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <HomeRoute />,
-      },
-      {
-        path: "dashboard",
+        path: "/",
         element: (
           <RequireAuth>
             <DashboardRoute />
